c00602b622b05fa76393e98c1311c8d350644d43
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2019-07-02 18:30:25 +0200

    css fixes

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('about/', views.about)
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about')
 ]
+
